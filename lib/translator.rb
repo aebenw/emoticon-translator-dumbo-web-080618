5edd2debe982588ef3ce9ba664e76eae0538ec1a
Andrew Woodward <ebenwoodward@icloud.com>
2018-08-01 15:23:25 +0000

    automatically backed up by learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -11,7 +11,7 @@ def load_library(file)
         if i == 0 
           new_hash["get_meaning"] = emoticons
         else 
-         new_hash["get_emoticons"] = emoticons
+         new_hash["get_emoticon"] = emoticons
        end
       # new_hash["get_meaning"] = define
       # new_hash["get_keys"] = define
