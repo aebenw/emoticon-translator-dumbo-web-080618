e8e30e85d6ee61c86cc9fa5611656b43d3e1f45d
Andrew Woodward <ebenwoodward@icloud.com>
2018-08-01 15:12:48 +0000

    automatically backed up by learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -5,16 +5,16 @@ new_hash = {}
 
 def load_library(file)
   emojis = YAML.load_file(file)
-  emojis.each do |define, syntax|
-    syntax.each do |english, japanese|
-      new_hash[get_meaning] = japanese
+  # emojis.each do |define, syntax|
+  #   syntax.each do |english, japanese|
+  #     new_hash[get_meaning] = japanese
       # new_hash[get_meaning][japanese] = define
       # new_hash[get_keys] = english 
       # new_hash[get_keys][english] = define
     end
   end
 end
-p new_hash
+p emojis
 
 def get_japanese_emoticon
   # code goes here
